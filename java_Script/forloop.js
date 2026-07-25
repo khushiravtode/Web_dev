3622708cf3ab81947b9c74dac53eb4afe12ff618
khushiravtode <ravtodekhushi@gmail.com>
2026-07-25 17:07:08 +0530

    Create forloop.js

---
+++ java_Script/forloop.js
@@ -0,0 +1,27 @@
+for(let i=0 ; i<=10;i++){
+    console.log(`gfgf ${i}`);
+    if(i==5){
+    break;                       //5 pachi loop mathi bar
+}
+}
+
+for(let i=0 ; i<=10;i++){
+    if(i==5){
+    continue;                   //==5 thay  tyare 5 skipp thi jay
+}
+console.log(`gfgf ${i}`);
+}
+
+index=1
+while (index<10){
+    console.log(`${index}`)
+    index=index+2
+}
+let myarr=[15,21,33,53,36,73,68]
+let i=0
+while(i<myarr.length)
+{
+    console.log(`arr ${myarr[i]}`)
+i=i+1
+
+}
