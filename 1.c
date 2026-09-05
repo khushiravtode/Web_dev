f99fc796ada182bab67e7e256cd9ff1fe08eb8a0
khushiravtode <ravtodekhushi@gmail.com>
2026-09-05 23:05:13 +0530

    .

---
+++ 1.c
@@ -0,0 +1,35 @@
+# # # # Example:
+# # # fruits = ["apple", "banana", "cherry"]
+ 
+# # # fruits.append("mango")
+# # # print(fruits)  # ['apple', 'banana', 'cherry', 'mango']
+ 
+# # # fruits.insert(1, "orange")
+# # # print(fruits)  # ['apple', 'orange', 'banana', 'cherry', 'mango']
+ 
+# # # fruits.remove("banana")
+# # # print(fruits)  # ['apple', 'orange', 'cherry', 'mango']
+ 
+# # # fruits.pop()
+# # # print(fruits)  # ['apple', 'orange', 'cherry']
+ 
+# # # fruits.pop(1)
+# # # print(fruits)  # ['apple', 'cherry']
+ 
+# # # fruits.sort()
+# # # print(fruits)  # ['apple', 'cherry']
+ 
+# # # fruits.reverse()
+# # # print(fruits)  # ['cherry', 'apple']
+ 
+# # # fruits.clear()
+# # # print(fruits)  # []
+ 
+# fruits = ["apple", "banana", "cherry", "apple"]
+# print(fruits.index("cherry"))  # 2
+# print(fruits.count("apple"))    # 2
+ 
+# # # fruits_copy = fruits.copy()
+# # # print(fruits_copy)  # ['apple', 'banana', 'cherry', 'apple']
+ 
+# # # fruits = ["apple", "banana", "cherry"]
